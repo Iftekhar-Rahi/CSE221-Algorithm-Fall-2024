1c083011b96d418b118260aa0d76d11008c6cb69
Iftekhar-Rahi <iftekharhossainrahi.gmail.com>
2024-12-15 18:56:12 +0600

    Revising Queue with built-in functions

--- My understanding over Graph/Revising Queue with built-in functions.py
+++ My understanding over Graph/Revising Queue with built-in functions.py
@@ -1,12 +1,6 @@
 from queue import Queue
-
-q = Queue()
-q.put(10)  # Enqueue
-q.put(20)  # Enqueue
-print(q.qsize())
-print(q.get())  # Dequeue: 10
-print(q.get())
-print(q.qsize())
-# for enqueue q.put(10)
-#for dequeue q.get()
-# for size q.qsize()
+my_queue=Queue()
+my_queue.put(5)         # Enqueue
+my_queue.put(10)        # Enqueue
+print(my_queue.get())   # Dequeue
+print(my_queue.qsize()) # Size
