1a0ee946bbbacf248d5b190825ed74626cc5c32e
Iftekhar-Rahi <iftekharhossainrahi.gmail.com>
2024-12-15 18:49:14 +0600

    Trying to understand Graph properly

---
+++ My understanding over Graph/Create graph from input file and print.py
@@ -0,0 +1,15 @@
+def create_graph_from_input_file_and_print():
+    with open("input.txt", "r") as infile:
+        vertex, edge = map(int, infile.readline().split())
+
+        graph = {}  # created a dictionary
+        for i in range(vertex + 1):  # creating all the vertex+1 (including 0) with a empty list
+            graph[i] = []
+
+        for i in range(edge):  # connecting all the edge
+            s, d, wt = map(int, infile.readline().split())
+            graph[s].append((d, wt))
+
+        for k, v in graph.items():  # printing the graph
+            print(k, v)
+create_graph_from_input_file_and_print()
--- My understanding over Graph/Revising Queue with built-in functions.py
+++ My understanding over Graph/Revising Queue with built-in functions.py
@@ -6,6 +6,7 @@ q.put(20)  # Enqueue
 print(q.qsize())
 print(q.get())  # Dequeue: 10
 print(q.get())
+print(q.qsize())
 # for enqueue q.put(10)
 #for dequeue q.get()
 # for size q.qsize()
